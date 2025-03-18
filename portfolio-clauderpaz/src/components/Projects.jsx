082d3cbd6c0db363bcf61b14993ad430f4afc0a3
Clauder Paz <142545565+ClauderPaz87@users.noreply.github.com>
2025-03-18 08:43:50 -0300

    atualizado

--- portfolio-clauderpaz/src/components/Projects.jsx
+++ portfolio-clauderpaz/src/components/Projects.jsx
@@ -17,7 +17,7 @@ const Projects = () => {
 
       <div className="grid lg:grid-cols-2 xl:grid-cols-3 md:grid-cols-1 lg:px-44 gap-8">
 
-        <Link href="https://spiffy-alpaca-3a137e.netlify.app/" 
+        <Link href="https://projects-twitterclone.vercel.app/" 
         className="hover:shadow-xl hover:shadow-shadowImage hover:-translate-y-3 hover:scale-105 
         transition-all ease-linear w-72 mx-auto">
           <div>
